a0195cc96e565d593cce6aefcb513bd35aef4807
maclean <gilleain.torrance@gmail.com>
2011-03-21 19:57:47 +0000

    Test all the molecules

--- src/test/kegg/KEGGMolTests.java
+++ src/test/kegg/KEGGMolTests.java
@@ -9,6 +9,7 @@ import org.openscience.cdk.geometry.cip.CIPTool.CIP_CHIRALITY;
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.interfaces.IStereoElement;
 import org.openscience.cdk.interfaces.ITetrahedralChirality;
+import org.openscience.cdk.smsd.labelling.AtomContainerPrinter;
 
 import stereo.CIPConfigurator;
 import test.BaseTest;
@@ -29,11 +30,68 @@ public class KEGGMolTests extends BaseTest {
                 System.out.println(atomIndex + ":" + chiral);
             }
         }
+        AtomContainerPrinter printer = new AtomContainerPrinter();
+        System.out.println(printer.toString(mol));
+    }
+    
+    @Test
+    public void C00002Test() throws FileNotFoundException, CDKException {
+        basicTest("C00002");
+    }
+    
+    @Test
+    public void C00008Test() throws FileNotFoundException, CDKException {
+        basicTest("C00008");
+    }
+    
+    @Test
+    public void C00019Test() throws FileNotFoundException, CDKException {
+        basicTest("C00019");
     }
     
     @Test
     public void C00025Test() throws FileNotFoundException, CDKException {
-        basicTest("C00025");
+        basicTest("C00002");
+    }
+    
+    @Test
+    public void C00026Test() throws FileNotFoundException, CDKException {
+        basicTest("C00026");
+    }
+    
+    @Test
+    public void C00036Test() throws FileNotFoundException, CDKException {
+        basicTest("C00036");
+    }
+    
+    @Test
+    public void C00041Test() throws FileNotFoundException, CDKException {
+        basicTest("C00041");
+    }
+    
+    @Test
+    public void C00049Test() throws FileNotFoundException, CDKException {
+        basicTest("C00049");
+    }
+    
+    @Test
+    public void C00170Test() throws FileNotFoundException, CDKException {
+        basicTest("C00170");
+    }
+    
+    @Test
+    public void C00263Test() throws FileNotFoundException, CDKException {
+        basicTest("C00263");
+    }
+    
+    @Test
+    public void C01879Test() throws FileNotFoundException, CDKException {
+        basicTest("C01879");
+    }
+    
+    @Test
+    public void C02926Test() throws FileNotFoundException, CDKException {
+        basicTest("C02926");
     }
 
 }
